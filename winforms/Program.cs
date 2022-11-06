96a126001d17392d021b9c0f4f51b4ff8fd1551f
apomic80 <apomic80@hotmail.com>
2022-11-06 11:59:09 +0100

    demo 1 done

--- winforms/Program.cs
+++ winforms/Program.cs
@@ -15,6 +15,7 @@ namespace winforms
         static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmDataVisualizer());
         }
